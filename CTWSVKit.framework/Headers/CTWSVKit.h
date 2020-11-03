f836837c72329bb10a22ddc567a65083dba5b7fe
tangxh <tangxh@chetong.net>
2020-11-03 16:10:55 +0800

    初始化framework

---
+++ CTWSVKit.framework/Headers/CTWSVKit.h
@@ -0,0 +1,19 @@
+//
+//  CTWSVKit.h
+//  CTWSVKit
+//
+//  Created by Tong CHE on 2020/11/3.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CTWSVKit.
+FOUNDATION_EXPORT double CTWSVKitVersionNumber;
+
+//! Project version string for CTWSVKit.
+FOUNDATION_EXPORT const unsigned char CTWSVKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CTWSVKit/PublicHeader.h>
+//#import <TXLiteAVSDK_TRTC/TRTCCloud.h>
+//#import "CTWTestClass.h"
+#import <CTWSVKit/CTWTestClass.h>
---
+++ CTWSVKit.framework/Headers/CTWTestClass.h
@@ -0,0 +1,20 @@
+//
+//  CTWTestClass.h
+//  CTWSVKit
+//
+//  Created by Tong CHE on 2020/11/3.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTWTestClass : NSObject
+
+@property (nonatomic, copy) NSString *name;
+
+@property (nonatomic, copy) NSString *address;
+
+@end
+
+NS_ASSUME_NONNULL_END
